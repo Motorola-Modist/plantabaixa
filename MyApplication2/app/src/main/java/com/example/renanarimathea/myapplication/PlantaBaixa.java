0df5e236cbf688bd89d4e2eddc78eb1f8be16880
Renan Arimathea <renan.arimathea@intellegobr.net>
2017-05-20 22:40:09 -0300

    plantabaixa

---
+++ MyApplication2/app/src/main/java/com/example/renanarimathea/myapplication/PlantaBaixa.java
@@ -0,0 +1,13 @@
+package com.example.renanarimathea.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantaBaixa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planta_baixa);
+    }
+}
